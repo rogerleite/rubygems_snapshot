fb9859231102fe024265b44b244d75d1b8637c98
Roger Leite <roger.barreto+github@gmail.com>
2010-04-17 20:10:24 -0300

    Added a fake environment to free tests

--- features/env.rb
+++ features/env.rb
@@ -7,6 +7,21 @@ Before do
   FileUtils.mkdir_p File.dirname(OUTPUT)
 end
 
+def create_fake_environment
+  ENV["GEM_HOME"]="/home/roger/ruby/rubygems_snapshot/tmp/fake_gems"
+  ENV["GEM_PATH"]="/home/roger/ruby/rubygems_snapshot/tmp/fake_gems"
+
+  FileUtils.rm_rf "tmp/fake_gems"
+  FileUtils.cp_r "fake_environment/", "tmp/fake_gems"
+  system "gem env"
+  system "gem list"
+  
+  system "gem build rubygems_snapshot.gemspec"
+  system "gem install rubygems_snapshot*.gem"
+
+  puts "===\n\n"
+end
+
 #execute system command, redirecting output
 def execute(command)
   system("#{command} > #{OUTPUT}")
@@ -20,9 +35,10 @@ end
 #Change some commands if necessary
 def gsub_command(system_command)
   command = system_command
-  if system_command.include?("gem ")
-    command.gsub!("gem ", "ruby -I ./lib/ `which gem` ")
-  end
+#  if system_command.include?("gem ")
+#    command.gsub!("gem ", "ruby -I ./lib/ `which gem` ")
+#  end
   command
 end
 
+create_fake_environment
